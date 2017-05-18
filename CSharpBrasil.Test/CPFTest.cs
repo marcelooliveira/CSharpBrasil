08b8ae28702b842c35b993b1d5a3317d475bbe75
marcelooliveira <mclricardo@gmail.com>
2017-05-18 01:03:01 -0300

    CSharpBrasil
    CSharpBrasil

---
+++ CSharpBrasil.Test/CPFTest.cs
@@ -0,0 +1,21 @@
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CSharpBrasil.Test
+{
+    [TestClass]
+    public class CPFTest
+    {
+        [TestMethod]
+        public void Test_23881228217_IsValid()
+        {
+            Assert.AreEqual(true, CPF.IsValid("23881228217"));
+        }
+
+        [TestMethod]
+        public void Test_25588580842_IsValid()
+        {
+            Assert.AreEqual(true, CPF.IsValid("25588580842"));
+        }
+    }
+}
