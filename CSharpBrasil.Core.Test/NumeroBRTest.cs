b66e7ec7568f387ea8c01ed9a4d6bca316e81b8b
marcelooliveira <mclricardo@gmail.com>
2017-05-21 00:57:37 -0300

    Resources

---
+++ CSharpBrasil.Core.Test/NumeroBRTest.cs
@@ -0,0 +1,89 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpBrasil.Core.Test
+{
+    [TestClass]
+    public class NumeroBRTest
+    {
+        NumeroBR numeroBR;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            numeroBR = new NumeroBR();
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void NegativoDeveGerarErro()
+        {
+            string extenso = numeroBR.Extenso(-1);
+        }
+
+        [TestMethod]
+        public void Test0()
+        {
+            string extenso = numeroBR.Extenso(0);
+            Assert.AreEqual("zero", extenso);
+        }
+
+        [TestMethod]
+        public void Test1()
+        {
+            string extenso = numeroBR.Extenso(1);
+            Assert.AreEqual("um", extenso);
+        }
+
+        [TestMethod]
+        public void Test9()
+        {
+            string extenso = numeroBR.Extenso(9);
+            Assert.AreEqual("nove", extenso);
+        }
+
+        [TestMethod]
+        public void Test10()
+        {
+            string extenso = numeroBR.Extenso(10);
+            Assert.AreEqual("dez", extenso);
+        }
+
+        [TestMethod]
+        public void Test17()
+        {
+            string extenso = numeroBR.Extenso(17);
+            Assert.AreEqual("dezessete", extenso);
+        }
+
+        [TestMethod]
+        public void Test20()
+        {
+            string extenso = numeroBR.Extenso(20);
+            Assert.AreEqual("vinte", extenso);
+        }
+
+        [TestMethod]
+        public void Test30()
+        {
+            string extenso = numeroBR.Extenso(30);
+            Assert.AreEqual("trinta", extenso);
+        }
+
+        [TestMethod]
+        public void Test90()
+        {
+            string extenso = numeroBR.Extenso(90);
+            Assert.AreEqual("noventa", extenso);
+        }
+
+        [TestMethod]
+        public void Test39()
+        {
+            string extenso = numeroBR.Extenso(39);
+            Assert.AreEqual("trinta e nove", extenso);
+        }
+    }
+}
---
+++ CSharpBrasil/NumeroBR.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Resources;
+using System.Text;
+using System.Globalization;
+using System.Threading;
+
+namespace CSharpBrasil
+{
+    public class NumeroBR
+    {
+        private const string NUMERO_NEGATIVO = "Número não pode ser negativo";
+        private readonly ResourceManager resourceManager;
+        public NumeroBR()
+        {
+            resourceManager = new ResourceManager(@"CSharpBrasil.Properties.Resources",
+                         System.Reflection.Assembly.Load(new System.Reflection.AssemblyName("CSharpBrasil")));
+        }
+
+        public string Extenso(int numero)
+        {
+            if (numero < 0)
+            {
+                throw new ArgumentOutOfRangeException(NUMERO_NEGATIVO);
+            }
+            else if (numero <= 20)
+            {
+                return resourceManager.GetString(string.Format("Extenso{0:000}", numero));
+            }
+            else if (numero > 20 && numero <= 99)
+            {
+                if (numero % 10 == 0)
+                {
+                    return resourceManager.GetString(string.Format("Extenso{0:000}", numero));
+                }
+                else
+                {
+                    var unidade = numero % 10;
+                    return string.Format("{0:000} e {1:000}"
+                        , resourceManager.GetString(string.Format("Extenso{0:000}", (numero / 10) * 10))
+                        , Extenso(unidade));
+                }
+            }
+            throw new NotImplementedException();
+        }
+    }
+}
